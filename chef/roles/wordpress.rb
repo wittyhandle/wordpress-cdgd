c11191ee69a7328b6004b2f8cc4ca61941c94db6
mottinger <mottinger@tacitknowledge.com>
2013-07-21 23:38:14 -0700

    added the wordpress recipe

--- chef/roles/wordpress.rb
+++ chef/roles/wordpress.rb
@@ -1,3 +1,3 @@
 name "wordpress"
 description "Nodes that run php with apache and mysql"
-run_list("role[base]", "role[php]", "recipe[mike_apache]", "recipe[apache2::mod_php5]", "role[database]")
+run_list("role[base]", "role[php]", "recipe[apache2::mod_php5]", "role[database]", "recipe[wordpress]")
