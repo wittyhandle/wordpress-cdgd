e57a6280af03948b3cee62b1dd31afe5214bf421
mottinger <mottinger@tacitknowledge.com>
2013-10-06 11:05:10 -0700

    added a global _log function, is this the best place for it?

--- main/wp-content/themes/bones/functions.php
+++ main/wp-content/themes/bones/functions.php
@@ -161,5 +161,16 @@ function bones_wpsearch($form) {
 	return $form;
 } // don't remove this bracket!
 
+if(!function_exists('_log')){
+  function _log( $message ) {
+    if( WP_DEBUG === true ){
+      if( is_array( $message ) || is_object( $message ) ){
+        error_log( print_r( $message, true ) );
+      } else {
+        error_log( $message );
+      }
+    }
+  }
+}
 
 ?>
