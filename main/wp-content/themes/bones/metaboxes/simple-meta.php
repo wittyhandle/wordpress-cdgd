13fc8771221f946a7373962769d5ed2a7b76792a
mottinger <mottinger@tacitknowledge.com>
2013-10-14 16:52:42 -0700

    got multiple media uploads working

--- main/wp-content/themes/bones/metaboxes/simple-meta.php
+++ main/wp-content/themes/bones/metaboxes/simple-meta.php
@@ -1,36 +1,38 @@
 <?php global $wpalchemy_media_access; ?>
 <div class="my_meta_control metabox">
   
-    <?php $mb->the_field('imgurl'); ?>
-    <?php $wpalchemy_media_access->setGroupName('nn')->setInsertButtonLabel('Insert'); ?>
- 
-    <div>
-        <?php echo $wpalchemy_media_access->getField(array('name' => $mb->get_the_name(), 'value' => $mb->get_the_value())); ?>
-        <?php echo $wpalchemy_media_access->getButton(); ?>
-						
-		<?php echo $wpalchemy_media_access->getIdField(array('class' => 'tout', 'name' => '_custom_meta[hidden_attachment_id]')); ?>				
-		
-		<?php
-			$attachment_id = $mb->get_the_value('hidden_attachment_id');
-			if ( isset($attachment_id) )
-			{
-				echo wp_get_attachment_image( $attachment_id, array(50, 50), false, array('class' => 'cdgd-selected-thumbnail') );
-			}
-			else
-			{?>
-				<img class="cdgd-selected-thumbnail">
-			<?php }
-		?>
+    <?php while($mb->have_fields_and_multi('heros')): ?>
+	<?php $mb->the_group_open(); ?>
+	
+		<?php $mb->the_field('imgurl'); ?>
+	    <?php $wpalchemy_media_access->setGroupName('hero-n' . $mb->get_the_index())->setInsertButtonLabel('Insert'); ?>
 
-    </div>
+	    <div>
+	        <a href="#" class="dodelete button">Remove</a>
+			
+			<?php echo $wpalchemy_media_access->getField(array('name' => $mb->get_the_name(), 'value' => $mb->get_the_value())); ?>
+	        <?php echo $wpalchemy_media_access->getButton(); ?>
+			<?php $mb->the_field('attachment_id'); ?>			
+			<?php echo $wpalchemy_media_access->getIdField(array('class' => 'tout', 'name' => $mb->get_the_name(), 'value' => $mb->get_the_value(), 'id' => 'attachment-' . $mb->get_the_index())); ?>
+		
+			<?php
+				$attachment_id = $mb->get_the_value();
+				if ( isset($attachment_id) )
+				{
+					echo wp_get_attachment_image( $attachment_id, array(50, 50), false, array('class' => 'cdgd-selected-thumbnail') );
+				}
+				else
+				{?>
+					<img class="cdgd-selected-thumbnail">
+				<?php }
+			?>
+
+	    </div>
  
-    <?php $mb->the_field('imgurl2'); ?>
-    <?php $wpalchemy_media_access->setGroupName('nn2')->setInsertButtonLabel('Insert This')->setTab('gallery'); ?>
- 
-    <p>
-        <?php echo $wpalchemy_media_access->getField(array('name' => $mb->get_the_name(), 'value' => $mb->get_the_value())); ?>
-        <?php echo $wpalchemy_media_access->getButton(array('label' => 'Add Image From Library')); ?>
-    </p>
+	<?php $mb->the_group_close(); ?>
+    <?php endwhile; ?>
+
+	<p><a href="#" class="docopy-heros button">Add</a></p>
  
 </div>
 
@@ -39,15 +41,16 @@
 	jQuery(function($)
 	{		
 		//<img width="32" height="27" src="http://localhost:1234/wp-content/uploads/2013/10/cdetorres_time_mobility_tout_01-150x129.png" class="attachment-32x32" alt="cdetorres_time_mobility_tout_01">
-		jQuery('input.tout').change(function()
+		jQuery('body').on('change', 'input.tout', function()
+		//jQuery('input.tout').change(function()
 		{
 			var that = jQuery(this);
-			console.log('val ' + jQuery('input.tout').val());
+			console.log('the id ' + that.attr('id'));
 			jQuery.post(
 				'/wp-admin/admin-ajax.php',
 				{
 					action: 'thumbnail-spec',
-					media_id: jQuery('input.tout').val(),
+					media_id: that.val(),
 					dimension: 50
 				},
 				function( response ) {
