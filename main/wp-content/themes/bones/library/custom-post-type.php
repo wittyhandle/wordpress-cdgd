44cd06d8ad33672941d7739faf4b3cf3756fc9a9
mottinger <mottinger@tacitknowledge.com>
2013-10-15 22:55:40 -0700

    defining the project categories taxonomy

--- main/wp-content/themes/bones/library/custom-post-type.php
+++ main/wp-content/themes/bones/library/custom-post-type.php
@@ -102,25 +102,25 @@ function custom_post_example() {
 	*/
 	
 	// now let's add custom categories (these act like categories)
-    register_taxonomy( 'custom_cat', 
-    	array('custom_type'), /* if you change the name of register_post_type( 'custom_type', then you have to change this */
-    	array('hierarchical' => true,     /* if this is true, it acts like categories */             
+    register_taxonomy( 'project_category', 
+    	array('project'),
+    	array('hierarchical' => true,
     		'labels' => array(
-    			'name' => __( 'Custom Categories', 'bonestheme' ), /* name of the custom taxonomy */
-    			'singular_name' => __( 'Custom Category', 'bonestheme' ), /* single taxonomy name */
-    			'search_items' =>  __( 'Search Custom Categories', 'bonestheme' ), /* search title for taxomony */
-    			'all_items' => __( 'All Custom Categories', 'bonestheme' ), /* all title for taxonomies */
-    			'parent_item' => __( 'Parent Custom Category', 'bonestheme' ), /* parent title for taxonomy */
-    			'parent_item_colon' => __( 'Parent Custom Category:', 'bonestheme' ), /* parent taxonomy title */
-    			'edit_item' => __( 'Edit Custom Category', 'bonestheme' ), /* edit custom taxonomy title */
-    			'update_item' => __( 'Update Custom Category', 'bonestheme' ), /* update title for taxonomy */
-    			'add_new_item' => __( 'Add New Custom Category', 'bonestheme' ), /* add new title for taxonomy */
-    			'new_item_name' => __( 'New Custom Category Name', 'bonestheme' ) /* name title for taxonomy */
+    			'name' => __( 'Project Categories', 'bonestheme' ), /* name of the custom taxonomy */
+    			'singular_name' => __( 'Project Category', 'bonestheme' ), /* single taxonomy name */
+    			'search_items' =>  __( 'Search Project Categories', 'bonestheme' ), /* search title for taxomony */
+    			'all_items' => __( 'All Project Categories', 'bonestheme' ), /* all title for taxonomies */
+    			'parent_item' => __( 'Parent Project Category', 'bonestheme' ), /* parent title for taxonomy */
+    			'parent_item_colon' => __( 'Parent Project Category:', 'bonestheme' ), /* parent taxonomy title */
+    			'edit_item' => __( 'Edit Project Category', 'bonestheme' ), /* edit custom taxonomy title */
+    			'update_item' => __( 'Update Project Category', 'bonestheme' ), /* update title for taxonomy */
+    			'add_new_item' => __( 'Add New Project Category', 'bonestheme' ), /* add new title for taxonomy */
+    			'new_item_name' => __( 'New Project Category Name', 'bonestheme' ) /* name title for taxonomy */
     		),
     		'show_admin_column' => true, 
     		'show_ui' => true,
     		'query_var' => true,
-    		'rewrite' => array( 'slug' => 'custom-slug' ),
+    		'rewrite' => array( 'slug' => 'project-slug' ),
     	)
     );   
     
