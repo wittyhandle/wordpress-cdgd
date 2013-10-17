32256ddb50770a5e9496870ca08db2b59ac80d1c
mottinger <mottinger@tacitknowledge.com>
2013-10-16 20:35:19 -0700

    don't use a secondary symlink when pointing to the wordpress files, it throws off WP plugins

--- chef/site-cookbooks/mike_wordpress/recipes/default.rb
+++ chef/site-cookbooks/mike_wordpress/recipes/default.rb
@@ -1,16 +1,5 @@
 include_recipe "wordpress"
 
-directory "#{node['wordpress']['dir']}/wp-content" do
-  recursive true
-  action :delete
-end
-
-#ln -s /wp-share/wp-content /var/www/wordpress/wp-content
-link "#{node['wordpress']['dir']}/wp-content" do
-  to "/wp-share"
-  action :create
-end
-
 # provide the .htaccess file for Wordpress to use
 cookbook_file "#{node['wordpress']['dir']}/.htaccess" do
   source ".htaccess"
