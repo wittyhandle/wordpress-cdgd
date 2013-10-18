1a8342134d1c51186cc72743e6feaa44fe556072
mottinger <mottinger@tacitknowledge.com>
2013-10-17 23:35:07 -0700

    rename the project metabox to project-hero

--- main/wp-content/themes/bones/metaboxes/project-hero-spec.php
+++ main/wp-content/themes/bones/metaboxes/project-hero-spec.php
@@ -4,6 +4,6 @@ $custom_metabox = $simple_mb = new WPAlchemy_MetaBox(array
 (
 	'id' => '_custom_meta',
 	'title' => 'Hero Images',
-	'template' => get_stylesheet_directory() . '/metaboxes/project-meta.php',
+	'template' => get_stylesheet_directory() . '/metaboxes/project-hero-meta.php',
 	'types' => array('project')
 ));
