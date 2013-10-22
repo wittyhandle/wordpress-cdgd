a61cacafc0afb5e9873180b13579b64aefc89fa2
mottinger <mottinger@tacitknowledge.com>
2013-10-21 22:21:05 -0700

    adding the tout ctp

--- main/wp-content/themes/bones/library/custom-post-type.php
+++ main/wp-content/themes/bones/library/custom-post-type.php
@@ -74,12 +74,45 @@ function add_custom_post_types() {
 			'query_var' => true,
 			'menu_position' => 4, /* this is what order you want it to appear in on the left hand side menu */ 
 			'menu_icon' => get_stylesheet_directory_uri() . '/library/images/icons/users.png', /* the icon for the custom post type menu */
-			'rewrite'	=> array( 'slug' => 'project', 'with_front' => false ), /* you can specify its url slug */
+			'rewrite'	=> array( 'slug' => 'client', 'with_front' => false ), /* you can specify its url slug */
 			'has_archive' => 'clients',
 			'capability_type' => 'page',
 			'hierarchical' => false,
 			/* the next one is important, it tells what's enabled in the post editor */
 			'supports' => array( 'title')
+	 	) /* end of options */
+	); /* end of register post type */
+	
+	register_post_type( 'tout',	 	
+		array( 'labels' => array(
+			'name' => __( 'Touts', 'bonestheme' ), /* This is the Title of the Group */
+			'singular_name' => __( 'Tout', 'bonestheme' ), /* This is the individual type */
+			'all_items' => __( 'Touts', 'bonestheme' ), /* the all items menu item */
+			'add_new' => __( 'Add New Tout', 'bonestheme' ), /* The add new menu item */
+			'add_new_item' => __( 'Add New Tout', 'bonestheme' ), /* Add New Display Title */
+			'edit' => __( 'Edit Touts', 'bonestheme' ), /* Edit Dialog */
+			'edit_item' => __( 'Edit Touts', 'bonestheme' ), /* Edit Display Title */
+			'new_item' => __( 'New Tout', 'bonestheme' ), /* New Display Title */
+			'view_item' => __( 'View Tout', 'bonestheme' ), /* View Display Title */
+			'search_items' => __( 'Search Touts', 'bonestheme' ), /* Search Custom Type Title */ 
+			'not_found' =>  __( 'No touts added.', 'bonestheme' ), /* This displays if there are no entries yet */ 
+			'not_found_in_trash' => __( 'No Touts in trash', 'bonestheme' ), /* This displays if there is nothing in the trash */
+			'parent_item_colon' => ''
+			), /* end of arrays */
+			'description' => __( 'Listing of touts to showcase your work', 'bonestheme' ), /* Custom Type Description */
+			'public' => true,
+			'publicly_queryable' => true,
+			'exclude_from_search' => false,
+			'show_ui' => true,
+			'query_var' => true,
+			'menu_position' => 4, /* this is what order you want it to appear in on the left hand side menu */ 
+			'menu_icon' => get_stylesheet_directory_uri() . '/library/images/icons/users.png', /* the icon for the custom post type menu */
+			'rewrite'	=> array( 'slug' => 'tout', 'with_front' => false ), /* you can specify its url slug */
+			'has_archive' => 'touts',
+			'capability_type' => 'page',
+			'hierarchical' => false,
+			/* the next one is important, it tells what's enabled in the post editor */
+			'supports' => array('title', 'editor')
 	 	) /* end of options */
 	); /* end of register post type */
 	
