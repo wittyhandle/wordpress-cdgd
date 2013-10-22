cb97f52f6b83435776f6a7c3cbc8a783031e9350
mottinger <mottinger@tacitknowledge.com>
2013-10-22 12:48:28 -0700

    finishing touches to length validation stuff

--- main/wp-content/themes/bones/library/js/wpalchemy-tout-lib.js
+++ main/wp-content/themes/bones/library/js/wpalchemy-tout-lib.js
@@ -1,21 +1,14 @@
 jQuery(function($)
 {		
-	console.log('called');
-	
 	jQuery('textarea.wp-editor-area').bind('input propertychange', function() 
 	{		
 		fd = jQuery('#faux-desc');
 		fd.html(jQuery(this).val());
 		
-		if (fd.height() > 80)
-		{
-			fd.css('background-color', '#ffbaba');
-		}
-		else
-		{
-			fd.css('background-color', '#dff2bf');
-		}
-		
+		fd.height() == 0 ? fd.hide() : fd.show()
+
+		var bg_color = fd.height() > 80 ? '#ffbaba' : '#dff2bf';				
+		fd.css('background-color', bg_color);				
 	});
 
 });
