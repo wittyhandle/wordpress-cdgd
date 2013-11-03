286965c8425a34e58ea7121ddf204c74601d9b4b
mottinger <mottinger@tacitknowledge.com>
2013-11-02 20:54:30 -0700

    render the project images

--- main/wp-content/themes/bones/single-project.php
+++ main/wp-content/themes/bones/single-project.php
@@ -1,3 +1,24 @@
 <?php get_header(); ?>
-single project
+
+<div class="clearfix project-details">
+	
+	<div id="menu_details">
+		left
+	</div>
+
+	<div id="heros">
+		<?php
+
+			$project_hero_metabox->the_meta();
+
+			while($project_hero_metabox->have_fields('heros'))
+			{
+				?><img src="<?php $project_hero_metabox->the_value('hero'); ?>"/><?php
+			}
+
+		?>
+	</div>
+	
+</div>
+
 <?php get_footer(); ?>
