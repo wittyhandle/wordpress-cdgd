8552863bdbc7b1cc4852dd720f4926b5d4fba170
mottinger <mottinger@tacitknowledge.com>
2013-11-07 20:44:04 -0800

    beginning to style navigation links on project detail page

--- main/wp-content/themes/bones/single-project.php
+++ main/wp-content/themes/bones/single-project.php
@@ -8,9 +8,31 @@
 			if ($cats)
 			{
 				$cat = array_pop($cats);
+				global $cat;
 				echo sprintf('<h1>%s</h1>', $cat->name);
 			}
+			
+			echo '<div>';
+			next_post_link("%link", "View Next");
+			echo '<span class="arrow-right"></span>';
+			echo '</div>';
+			
+			echo '<div>';
+			previous_post_link("%link", "View Previous");
+			echo '<span class="arrow-left"></span>';
+			echo '</div>';
+			
+			echo '<div>';
+			echo 'View All ';
+			//$cat = array_pop($cats);
+			echo $cat->name;
+			echo '</div>';
 		?>
+		
+		<div>
+			View All
+		</div>
+		
 	</nav>
 
 	<div id="heros">
